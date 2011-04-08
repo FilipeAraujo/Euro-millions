fa4581a427a9f3f127b2a81da49ce4e50177f515
Filipe Araujo <7.filipe@gmail.com>
2011-04-08 18:47:26 +0100

    ínicio

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,76 @@
+class HomeController < ApplicationController
+  def search
+    @stuff = {:name => "dsadaadsa"}
+    puts params
+    respond_to do |format|
+      format.html { render :html => @stuff}
+      format.xml  { render :xml => @stuff }
+      format.json { render :json => @stuff }
+    end
+    
+  end
+  
+  def index
+    
+    if(params[:name] == nil)
+        return 
+    end
+        
+    @resposta = String::new
+    
+    a = params[:name].split.collect! { |i| i.to_i }
+    
+    xml = 'EuroMilhoesAno.xml'
+    
+    url = 'http://services.sapo.pt/Sport/Score/'+xml
+    xml_data = Net::HTTP.get_response(URI.parse(url)).body
+
+    data = XmlSimple.xml_in(xml_data)
+
+    premios = {
+      "5,2" => "1",
+      "5,1" => "2",
+      "5,0" => "3",
+      "4,2" => "4",
+      "4,1" => "5",
+      "4,0" => "6",
+      "3,2" => "7",
+      "3,1" => "8",
+      "2,2" => "9",
+      "3,0" => "10",
+      "1,2" => "11",
+      "2,1" => "12"
+
+    }
+
+
+    data['Item'].each do |item|
+
+      keys = ["Num_1", "Num_2", "Num_3", "Num_4", "Num_5", "Est_1", "Est_2"]
+
+      b = Array.new
+
+      keys.each do |key|
+        b.push(item[key][0].to_i)
+      end
+
+      numeros = (a[0..4] & b[0..4]).count 
+      estrelas = (a[5..6] & b[5..6]).count 
+      comb = numeros.to_s + "," + estrelas.to_s
+      resultado = premios[comb]
+
+      if( resultado != nil)
+         @resposta += "<p>"
+         @resposta += "Concurso => " + item["Concurso"][0] + " Premio: " + resultado  + " Valor - " + item['Pre_'+resultado+'_Val'][0] + " " + comb + "</hr>"
+         @resposta += "</p>"
+      end  
+end   
+ 
+    #@euro = { :name => "ola"}
+    
+    respond_to do |format|
+      format.html { render :html => @resposta }# show.html.erb
+    end
+    
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Euro::Application.config.secret_token = 'c313e63f8806de7142007f57e9072dbdb96a4edf9109c504711abab023cd3c351330f34544fe86d81d15ff0eb9d87c58d52e29ac1acebc9291ef084727faa8bd'
